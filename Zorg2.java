46ed588469adc255e8d21b721f2dc0ed8b172e35
Aka_Ron4uk <oleg_1987@mail.ru>
2020-02-25 15:26:35 +0300

    Сортировка Merge

---
+++ Zorg2.java
@@ -0,0 +1,75 @@
+package forGit.Java;
+
+/**
+ * Created by Oleg on 03.08.2019.
+ */
+
+
+
+
+public class Zorg2 {
+    private  String name;
+    public static void main(String[] args) {
+        int[] massif = {13, 3, 8, 1, 15, 2, 3, 7, 4};
+        System.out.print("Массив до сортировки: ");
+        for (int i = 0; i < massif.length; i++)
+            System.out.print(massif[i] + " ");
+        System.out.println("");
+        massif = sortArray(massif);
+        System.out.print("Массив после сортировки: ");
+        for (int i = 0; i < massif.length; i++)
+            System.out.print(massif[i] + " ");
+    }
+
+
+    public static int[] sortArray(int[] arrayA) { // сортировка Массива который передается в функцию
+        // проверяем не нулевой ли он?
+        if (arrayA == null) {
+            return null;
+        }
+        // проверяем не 1 ли элемент в массиве?
+        if (arrayA.length < 2) {
+            return arrayA; // возврат в рекурсию в строки ниже см комменты.
+        }
+        // копируем левую часть от начала до середины
+        int[] arrayB = new int[arrayA.length / 2];
+        System.arraycopy(arrayA, 0, arrayB, 0, arrayA.length / 2);
+
+        // копируем правую часть от середины до конца массива, вычитаем из длины первую часть
+        int[] arrayC = new int[arrayA.length - arrayA.length / 2];
+        System.arraycopy(arrayA, arrayA.length / 2, arrayC, 0, arrayA.length - arrayA.length / 2);
+
+        // рекурсией закидываем поделенные обе части обратно в наш метод, он будет крутится до тех пор,
+        // пока не дойдет до 1 элемента в массиве, после чего вернется в строку и будет искать второй такой же,
+        // точнее правую часть от него и опять вернет его назад
+        arrayB = sortArray(arrayB); // левая часть возврат из рекурсии строкой return arrayA;
+        arrayC = sortArray(arrayC); // правая часть возврат из рекурсии строкой return arrayA;
+
+        // далее опять рекурсия возврата слияния двух отсортированных массивов
+        return mergeArray(arrayB, arrayC);
+    }
+
+    public static int[] mergeArray(int[] arrayА, int[] arrayB) {
+
+        int[] arrayC = new int[arrayА.length + arrayB.length];
+        int positionA = 0, positionB = 0;
+
+        for (int i = 0; i < arrayC.length; i++) {
+            if (positionA == arrayА.length) {
+                arrayC[i] = arrayB[positionB];
+                positionB++;
+            } else if (positionB == arrayB.length) {
+                arrayC[i] = arrayА[positionA];
+                positionA++;
+            } else if (arrayА[positionA] < arrayB[positionB]) {
+                arrayC[i] = arrayА[positionA];
+                positionA++;
+            } else {
+                arrayC[i] = arrayB[positionB];
+                positionB++;
+            }
+        }
+        return arrayC;
+    }
+}
+
