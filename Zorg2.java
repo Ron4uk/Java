befcdfa699d11c22d76273b06e6db012c7a99a29
Aka_Ron4uk <oleg_1987@mail.ru>
2020-02-25 15:31:45 +0300

    Merge

--- Zorg2.java
+++ Zorg2.java
@@ -9,7 +9,6 @@ package forGit.Java;
 
 public class Zorg2 {
     private  String name;
-
     public static void main(String[] args) {
         int[] massif = {13, 3, 8, 1, 15, 2, 3, 7, 4};
         System.out.print("Массив до сортировки: ");
